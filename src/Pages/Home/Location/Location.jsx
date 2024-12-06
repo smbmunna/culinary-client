b293eb73321422b5d29a2a4a0a633618a2131828
smbmunna <smbmunna@gmail.com>
2024-12-06 18:19:24 +0600

    FAQ section added in the find us components

--- src/Pages/Home/Location/Location.jsx
+++ src/Pages/Home/Location/Location.jsx
@@ -1,14 +1,24 @@
-
-import React from "react"
 import { Map, Marker } from "pigeon-maps"
+import FAQ from "../../../Components/FAQ/FAQ";
 
-//23.815777805337525, 90.41691067949965
+
+//23.815777805337525, 90.41691067949965 dhaka
 
 const Location = () => {
     return (
-        <Map height={300} defaultCenter={[23.815777805337525, 90.41691067949965]} defaultZoom={11}>
-            <Marker width={50} anchor={[23.815777805337525, 90.41691067949965]} />
-        </Map>
+        <div>
+            <h1 className="font-bold text-5xl mb-8 text-center pt-16">Our Location / FAQ</h1>
+            <div className="grid grid-cols-2 gap-4 items-center">
+                <div>
+                    <Map height={300} defaultCenter={[23.815777805337525, 90.41691067949965]} defaultZoom={11}>
+                        <Marker width={50} anchor={[23.815777805337525, 90.41691067949965]} />
+                    </Map>
+                </div>
+                <div>
+                    <FAQ />
+                </div>
+            </div>
+        </div>
     );
 };
 
