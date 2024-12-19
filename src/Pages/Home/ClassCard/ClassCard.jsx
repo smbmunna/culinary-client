87b96387eb0f951200300abf6deb2c679d201c32
smbmunna <smbmunna@gmail.com>
2024-12-19 23:56:11 +0600

    Classes card modified

--- src/Pages/Home/ClassCard/ClassCard.jsx
+++ src/Pages/Home/ClassCard/ClassCard.jsx
@@ -1,6 +1,7 @@
+import { FaCalendarAlt , FaMoneyBillAlt} from "react-icons/fa";
 
 const ClassCard = ({ cls }) => {
-    const { title, price, description, fee, image } = cls;
+    const { title, price, description, fee, image, time } = cls;
     return (
         <div className="bg-base-100 px-4 py-4">
             <figure>
@@ -9,6 +10,18 @@ const ClassCard = ({ cls }) => {
                     src={image}
                     alt="Class" />
             </figure>
+            <div className="flex  items-center mt-2 gap-80 bg-black text-white py-2 px-2">
+                <div className="flex gap-2">
+                    <p>Class Starts: </p>
+                    <span><FaCalendarAlt className="text-lg text-[#fe3f00]" /></span>
+                    <span>{time}</span>
+                </div>
+                <div className="flex gap-2">
+                    <FaMoneyBillAlt className="text-xl text-[#fe3f00]"/>
+                    <span>{fee}</span>
+                </div>
+            </div>
+
             <div className="mt-4 mb-8">
                 <h2 className="card-title mb-2">{title}</h2>
                 <p className="text-gray-600">{description}</p>
