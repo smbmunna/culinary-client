09020f5b512ae1b30d3b7de534a2d5fb851a0298
smbmunna <smbmunna@gmail.com>
2024-12-20 12:35:09 +0600

    Classes section modified

--- src/Pages/Home/ClassCard/ClassCard.jsx
+++ src/Pages/Home/ClassCard/ClassCard.jsx
@@ -1,28 +1,28 @@
-import { FaCalendarAlt , FaMoneyBillAlt} from "react-icons/fa";
+import { FaCalendarAlt, FaMoneyBillAlt } from "react-icons/fa";
 
 const ClassCard = ({ cls }) => {
     const { title, price, description, fee, image, time } = cls;
     return (
-        <div className="bg-base-100 px-4 py-4">
-            <figure>
+        <div className="bg-base-100 px-16 md:px-4 py-4">
+            <div className="">
                 <img
                     className=""
                     src={image}
                     alt="Class" />
-            </figure>
-            <div className="flex  items-center mt-2 gap-80 bg-black text-white py-2 px-2">
-                <div className="flex gap-2">
-                    <p>Class Starts: </p>
-                    <span><FaCalendarAlt className="text-lg text-[#fe3f00]" /></span>
-                    <span>{time}</span>
-                </div>
-                <div className="flex gap-2">
-                    <FaMoneyBillAlt className="text-xl text-[#fe3f00]"/>
-                    <span>{fee}</span>
-                </div>
             </div>
 
-            <div className="mt-4 mb-8">
+            <div className="mb-8">
+                <div className="flex justify-between items-center bg-black text-white py-2 px-2 mb-2">
+                    <div className="flex gap-2">
+                        {/* <p>Class Starts: </p> */}
+                        <span><FaCalendarAlt className="text-lg text-[#fe3f00]" /></span>
+                        <span>{time}</span>
+                    </div>
+                    <div className="flex gap-2">
+                        <FaMoneyBillAlt className="text-xl text-[#fe3f00]" />
+                        <span>{fee}</span>
+                    </div>
+                </div>
                 <h2 className="card-title mb-2">{title}</h2>
                 <p className="text-gray-600">{description}</p>
                 <div className="mt-2">
