54c767d88273ba34c2ef0ac97c6bf1a00996a0c8
smbmunna <smbmunna@gmail.com>
2024-12-27 15:03:37 +0600

    Course detail page modified

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -5,24 +5,29 @@ import { MdEmail } from "react-icons/md";
 
 const ContactUs = () => {
     return (
-        <div className="pt-20 grid grid-cols-1 md:grid-cols-2">
+        <div className="pt-20 grid grid-cols-1 md:grid-cols-2 items-center">
             {/* form */}
-            <div className="grid gap-4 w-1/2 mx-auto md:w-3/4">
-                <label className="input input-bordered flex items-center gap-2">
-                    Name
-                    <input type="text" className="grow" placeholder="Daisy" />
-                </label>
-                <label className="input input-bordered flex items-center gap-2">
-                    Email
-                    <input type="text" className="grow" placeholder="daisy@site.com" />
-                </label>
-                <textarea className="textarea textarea-bordered" placeholder="Bio"></textarea>
+            <div>
+                <h1 className="font-bold text-5xl text-center text-gray-700  font-RobotoCondensed">Write to Us</h1>
+                <div className="grid gap-4 w-1/2 mx-auto md:w-3/4">
+                    <label className="input input-bordered flex items-center gap-2">
+                        Name
+                        <input type="text" className="grow" placeholder="Daisy" />
+                    </label>
+                    <label className="input input-bordered flex items-center gap-2">
+                        Email
+                        <input type="text" className="grow" placeholder="daisy@site.com" />
+                    </label>
+                    <textarea className="textarea textarea-bordered" placeholder="Your Message"></textarea>
 
-                <button className="bg-[#fe3f00] text-white px-8 py-2 text-lg hover:bg-black" >Send Message</button>
+                    <button className="bg-[#fe3f00] text-white px-8 py-2 text-lg hover:bg-black" >Send Message</button>
+                </div>
             </div>
             {/* map and location */}
+
             <div className="mt-8 md:mt-0 w-1/2 mx-auto md:w-2/3">
                 <div className="mb-8 md:mb-0">
+                    <h1 className="font-bold text-5xl text-center md:py-8 text-gray-700  font-RobotoCondensed">Our Location</h1>
                     <Map height={300} defaultCenter={[23.815777805337525, 90.41691067949965]} defaultZoom={11}>
                         <Marker width={50} anchor={[23.815777805337525, 90.41691067949965]} />
                     </Map>
--- src/Pages/CourseDetail/CourseDetail.jsx
+++ src/Pages/CourseDetail/CourseDetail.jsx
@@ -5,23 +5,32 @@ import useFetch from "../../Hooks/useFetch";
 const CourseDetail = () => {
     const { id } = useParams();
     const [data, loading, error] = useFetch('/classes.json');
-    if(loading){
+    if (loading) {
         return <div className="text-2xl text-red-200">Loading...</div>
     }
-    
-    const classInfo= data.filter(cls=>cls.id==id);
-    
-    const { title, description, fee, image, time } = classInfo[0];
+
+    const classInfo = data.filter(cls => cls.id == id);
+
+    const { title, description, fee, image, time, duration, availability, location, notes } = classInfo[0];
 
 
     return (
-        <div className="">
-            <div className="grid gap-4  top-20 px-4">
-                <h1 className="text-5xl">{title}</h1>
+        <div className="pt-24 grid md:grid-cols-2 max-w-screen-lg mx-auto items-center ">
+            <div className="grid top-20 px-4">
+                <h1 className="text-5xl  text-gray-700 font-bold font-RobotoCondensed">{title}</h1>
                 <img src={image} alt="" />
-                <p className="text-gray-500">{description}</p>
-                <p className="text-gray-500">{fee}</p>
-                <p className="text-gray-500">{time}</p>
+                <h2 className="text-2xl mt-4  text-gray-700 font-bold font-RobotoCondensed">Highlights of the Cooking Class</h2>
+                <p className="text-gray-500 py-2">{description}</p>
+                <p className="text-gray-500 font-bold"><span className="font-bold text-black">Fees: </span>{fee}</p>
+                <p className="text-gray-500  font-semibold"><span className="font-bold text-black">Duration: </span>{duration}</p>
+                <p className="text-gray-500  font-semibold"><span className="font-bold text-black">Availability: </span>{availability}</p>                
+                <p className="text-gray-500 font-semibold"><span className="font-bold text-black">Location: </span>{location}</p>                
+            </div>
+            {/* sidebar */}
+            <div className="sticky">
+                <h1 className="text-5xl  text-gray-700 font-bold font-RobotoCondensed">Want to be a Chef?</h1>
+                <img className="px-8" src="https://i.ibb.co.com/ynCXkWy/chef3.jpg" alt="" />
+                <button className="bg-[#fe3f00] text-white px-8 py-2 text-lg hover:bg-black mt-2 w-4/3 grid mx-auto" >Contact Us</button>
             </div>
         </div>
     );
